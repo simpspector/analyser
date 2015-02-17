a8b5fdf0c463336f605d42d468e8f4a1189b03a3
DavidBadura <d.badura@gmx.de>
2015-02-17 21:45:20 +0000

    add twig lint

---
+++ src/Gadget/TwigLintGadget.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace SimpSpector\Analyser\Gadget;
+
+use Asm89\Twig\Lint\StubbedEnvironment;
+use SimpSpector\Analyser\Issue;
+use SimpSpector\Analyser\Logger\AbstractLogger;
+use SimpSpector\Analyser\Result;
+
+/**
+ * @author Tobias Olry <tobias.olry@gmail.com>
+ */
+class TwigLintGadget extends AbstractGadget
+{
+    const NAME = 'twig_lint';
+
+    /**
+     * @var StubbedEnvironment
+     */
+    private $twig;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->twig = new StubbedEnvironment(new \Twig_Loader_String());
+    }
+
+    /**
+     * @param string $path
+     * @param array $options
+     * @param AbstractLogger $logger
+     * @return Result
+     */
+    public function run($path, array $options, AbstractLogger $logger)
+    {
+        $options = $this->prepareOptions(
+            $options,
+            [
+                'files'       => ['.'],
+                'error_level' => 'error',
+            ],
+            ['files']
+        );
+
+        $result = new Result();
+        $files  = $this->findFiles($path, $options['files'], '*.twig');
+
+        foreach ($files as $file) {
+            try {
+                $this->twig->parse($this->twig->tokenize(file_get_contents($file), $file));
+            } catch (\Twig_Error $e) {
+                $cleanedUpFile = $this->cleanupFilePath($path, $file);
+                $message       = get_class($e) . ': ' . $e->getRawMessage();
+
+                $issue = new Issue($message, self::NAME, $options['error_level']);
+                $issue->setFile($cleanedUpFile);
+                $issue->setLine($e->getTemplateLine());
+
+                $result->addIssue($issue);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
---
+++ tests/Gadget/TwigLintGadgetTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SimpSpector\Analyser\Tests\Gadget;
+
+use SimpSpector\Analyser\Issue;
+use SimpSpector\Analyser\Gadget\TwigLintGadget;
+use SimpSpector\Analyser\Logger\NullLogger;
+
+/**
+ * @author Tobias Olry <tobias.olry@gmail.com>
+ */
+class TwigLintGadgetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNoErrors()
+    {
+        $path   = __DIR__ . '/_data/twig_lint/success';
+        $config = [];
+
+        $gadget = new TwigLintGadget();
+        $issues = $gadget->run($path, $config, new NullLogger())->getIssues();
+
+        $this->assertEquals([], $issues);
+    }
+
+    public function testOneLineError()
+    {
+        $path   = __DIR__ . '/_data/twig_lint/error';
+        $config = [];
+
+        $gadget = new TwigLintGadget();
+        $issues = $gadget->run($path, $config, new NullLogger())->getIssues();
+
+        $expectedIssues = [
+            $this->createIssue('Twig_Error_Syntax: Unclosed "block"', 'one_line_error.html.twig', 11, 'error'),
+        ];
+
+        $this->assertEquals($expectedIssues, $issues);
+    }
+
+    private function createIssue($message, $file, $line, $level)
+    {
+        $issue = new Issue($message, TwigLintGadget::NAME, $level);
+        $issue->setFile($file);
+        $issue->setLine($line);
+
+        return $issue;
+    }
+}
