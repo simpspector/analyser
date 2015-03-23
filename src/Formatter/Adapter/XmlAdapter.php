1b63852ce67619055cffa006fc3072c39f6bc795
Robin Willig <robin@dragonito.net>
2015-03-23 19:19:00 +0100

    Refaktor, used SerializerAdapter

--- src/Formatter/Adapter/XmlAdapter.php
+++ src/Formatter/Adapter/XmlAdapter.php
@@ -4,21 +4,18 @@ namespace SimpSpector\Analyser\Formatter\Adapter;
 
 use SimpSpector\Analyser\Result;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 /**
  * @author Robin Willig <robin@dragonito.net>
  */
-class XmlAdapter implements AdapterInterface
+class XmlAdapter extends SerializerAdapter
 {
     /**
      * @param Result $result
      */
     public function format(Result $result)
     {
-        $serializer = new Serializer([new GetSetMethodNormalizer()], [new XmlEncoder()]);
-        echo $serializer->serialize($result, 'xml');
+        return $this->serializer->serialize($result, 'xml');
     }
 
     /**
