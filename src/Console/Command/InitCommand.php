1b61987a56558167f97422bd5a2b53f80464097a
Tobias Olry <tobias.olry@gmail.com>
2017-03-11 13:10:20 +0100

    first runnable version \o/

---
+++ src/Console/Command/InitCommand.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace SimpSpector\Analyser\Console\Command;
+
+use SimpSpector\Analyser\Analyser;
+use SimpSpector\Analyser\Formatter\FormatterInterface;
+use SimpSpector\Analyser\Logger\ConsoleLogger;
+use SimpSpector\Analyser\Repository\RepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Yaml\Dumper;
+use Symfony\Component\Filesystem\Filesystem;
+
+/*
+ * @author Tobias Olry <tobias.olry@gmail.com>
+ */
+class InitCommand extends Command
+{
+    /**
+     * @var RepositoryInterface
+     */
+    private $gadgetRepository;
+
+    /**
+     * @param Analyser $analyser
+     */
+    public function __construct(RepositoryInterface $gadgetRepository)
+    {
+        parent::__construct();
+
+        $this->gadgetRepository = $gadgetRepository;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('init')
+            ->setDescription('setup configuration for new project')
+            ->addArgument('path', InputArgument::OPTIONAL, 'path to the project', '.');
+    }
+
+    /**
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     * @throws \Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filesystem = new Filesystem();
+        $yamlDumper = new Dumper();
+        $gadgets = $this->gadgetRepository->all();
+        $questionHelper = $this->getHelper('question');
+
+        $folder = realpath($input->getArgument('path'));
+        $configFile = "$folder/.simpspector.yml";
+        $configFileData = [];
+
+        if ($filesystem->exists($configFile)) {
+            throw new \Exception('simpspector already configured');
+        }
+
+        $output->writeln("config file target\n\t<info>$configFile</info>");
+
+        foreach ($gadgets as $key => $gadget) {
+            $question = new ConfirmationQuestion("[$key] enable? (Y/n) ");
+
+            if ($questionHelper->ask($input, $output, $question)) {
+                $configFileData[$key] = null;
+            }
+        }
+
+        if (! empty($configFileData)) {
+            $yamlData = $yamlDumper->dump($configFileData, $inline = 3);
+            $filesystem->dumpFile($configFile, $yamlData);
+            $output->writeln("<info>success</info> config file written");
+        }
+    }
+}
